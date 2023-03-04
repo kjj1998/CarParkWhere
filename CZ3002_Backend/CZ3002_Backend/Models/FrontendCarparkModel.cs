ba27e7cc4c2d3a0ff0cc7429b401c2a6408c335b
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-03-04 23:21:51 +0800

    removed previously added frontend models

--- CZ3002_Backend/CZ3002_Backend/Models/FrontendCarparkModel.cs
+++ CZ3002_Backend/CZ3002_Backend/Models/FrontendCarparkModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json.Serialization;
-using Google.Cloud.Firestore;
 
 namespace CZ3002_Backend.Models;
 
@@ -15,46 +14,4 @@ public class FrontendCarparkModel
         Hdb = hdb;
         Ura = ura;
     }
-}
-
-public class FrontendMallCarparkModel
-{
-    public string? Name { get; set; }
-    public string? CarparkCode { get; set; }
-    public LatLong? Coordinates { get; set; }
-    public string? WeekDayRate1 { get; set; }
-    public string? WeekDayRate2 { get; set; }
-    public string? SatRate { set; get; }
-    public string? SunPhRate { set; get; }
-    public Lots? Lots { set; get; }
-
-    // public FrontendMallCarparkModel(string Name, )
-    // {
-    //     
-    // }
-}
-
-public class FrontendHdbCarparkModel
-{
-    public string? CarparkCode { get; set; }
-    public string? Name { get; set; }
-    public string? System { get; set; }
-    public Lots? Lots { get; set; }
-    public LatLong? Coordinates { get; set; }
-    public string? ShortTermParking { get; set; }
-    public string? FreeParking { get; set; }
-    public string? NightParking { get; set; }
-    public int CarParkDecks { get; set; }
-    public float GantryHeight { get; set; }
-    public string? CarParkBasement { get; set; }
-}
-
-public class FrontendUraCarparkModel
-{
-    public string? CarparkCode { get; set; }
-    public string? Name { get; set; }
-    public LatLong? Coordinates { get; set; }
-    public string? System { get; set; }
-    public Lots? Lots { get; set; }
-    public List<CarparkRate>? Rates { get; set; }
 }
